c93146922e0ffdaf0bb0857bc9a430038feb7bc4
Ned-el-ch <nedelchev.niki@gmail.com>
2019-10-17 15:36:09 +0100

    m

--- lib/student.rb
+++ lib/student.rb
@@ -2,8 +2,75 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]
+    attr_accessor :name, :grade
+    attr_reader :id
 
+    def initialize(name, grade, id = nil)
+
+        @name = name
+        @grade = grade
+        @id = id
+        
+    end
+
+    def self.create_table
+
+        sql = <<~SQL
+
+            CREATE TABLE IF NOT EXISTS students (
+
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                grade TEXT
+
+            );
+        
+        SQL
+
+        DB[:conn].execute(sql)
+        
+    end
+
+    def self.drop_table
+
+        sql = <<~SQL
+
+            DROP TABLE IF EXISTS students;
+        
+        SQL
+        
+        DB[:conn].execute(sql)
+
+    end
+
+    def save
+
+
+        
+    end
+
+    def self.create
+
+
+        
+    end
+
+    def self.new_from_db(row)
+
+
+        
+    end
+
+    def self.find_by_name(name)
+
+
+        
+    end
+
+    def update
+
+        
+        
+    end
 
 end
